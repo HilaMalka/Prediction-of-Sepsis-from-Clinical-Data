ecbc5901a736bb1e2ec32b77d6fc54fb9cba321a
EyalMarantz <88968428+EyalMarantz@users.noreply.github.com>
2023-05-11 18:50:30 +0300

    Add files via upload

--- predict.py
+++ predict.py
@@ -12,13 +12,17 @@ if __name__ == '__main__':
     path = args.input_folder
 
     X_test, y_test, ids = preprocess(path, 'comp')
+
+    index = [int(i[i.find('patient_') + len('patient_'):]) for i in ids]
+
     model = XGBClassifier()
     model.load_model('comp.json')
 
     pred = model.predict(X_test)
 
     res = pd.DataFrame.from_dict({'id': ids,
-                                  'prediction': pred})
-
+                                  'prediction': pred,
+                                  'index': index})
+    res = res.sort_values('index').drop(['index'], axis=1)
     pd.DataFrame.to_csv(res, 'prediction.csv', index=False)
 
